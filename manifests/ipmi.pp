953936e1cd04f64e5b38921ba06e6aa02e165a0d
John Van de Meulebrouck Brendgard <john@sunet.se>
2018-08-24 18:07:08 +0200

    Added functionality for:
    Disable NONE etc authentication by only allowing MD5
    Disable IPMI access for all common, default users

--- manifests/ipmi.pp
+++ manifests/ipmi.pp
@@ -7,11 +7,22 @@ class bastion::ipmi {
 
         if $bastion::ipmi_lan_access_off {
             exec { 'ipmitool lan set 1 access off':
-                unless => 'ipmitool channel info 1 | egrep -o "[[:space:]]+Access[[:space:]]Mode[[:space:]]+:[[:space:]]disabled"'
+                unless => 'ipmitool channel info 1 | egrep -o "[[:space:]]+Access Mode[[:space:]]+: disabled"'
             }
         }
 
-        # ipmiutil is only available on Ubuntu > 14.04
+        # Disable NONE etc authentication by only allowing MD5
+        exec { 'ipmitool lan set 1 auth Callback MD5; ipmitool lan set 1 auth User MD5; ipmitool lan set 1 auth Operator MD5; ipmitool lan set 1 auth Admin MD5; ipmitool lan set 1 auth OEM MD5':
+            onlyif => 'ipmitool lan print | egrep "NONE|MD2|PASSWORD"'
+        }
+
+        # Disable IPMI access for all common, default users
+        exec { 'bash -c \'if [[ -n "$(ipmitool user summary 1 | egrep -o "Enabled User Count[[:space:]]+: [1-9]+")" ]]; then admin_user="$(ipmitool user list 1 | egrep "[1-9]+[[:space:]]+ADMIN|root|admin|Administrator|USERID" | egrep -o "^[0-9]+")"; ipmitool user disable "${admin_user}"; fi\'': }
+
+        # ipmiutil lan -d "disables the IPMI LAN and PEF parameters,
+        # so as not to allow BMC LAN connections or alerts.
+        # This option  also  sets the IP address to zeros."
+        # However ipmiutil is only available on Ubuntu > 14.04
         if $::operatingsystem == 'Ubuntu' and $::operatingsystemrelease != '14.04' and $::operatingsystemrelease != '12.04' {
             package { 'ipmiutil': } ->
             exec { 'ipmiutil lan -d':
